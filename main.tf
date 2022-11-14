08da365a2b2bd429429416006b13f3b862bfc752
k-double <67038653+k-double@users.noreply.github.com>
2022-11-13 23:25:26 -0500

    security

--- main.tf
+++ main.tf
@@ -14,6 +14,10 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.16.0"
